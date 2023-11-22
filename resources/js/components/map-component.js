e7c03cdfcca1a6bfb2802c90605db26517a4e641
Francesco <inerba@gmail.com>
2023-11-22 16:38:51 +0100

    wip

--- resources/js/components/map-component.js
+++ resources/js/components/map-component.js
@@ -249,7 +249,16 @@ export default function mapComponent({
 
         loadGeoJson() {
             const layer = L.geoJSON(JSON.parse(geoJsonFeature));
+
+            console.log(layer);
+
             geoJsonGroup.addLayer(layer);
+
+            if (geoJsonGroup.hasLayer(layer)) {
+                console.log("Il layer è presente nel gruppo");
+            } else {
+                console.log("Il layer non è presente nel gruppo");
+            }
         },
 
         init: function () {
