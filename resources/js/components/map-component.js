b3d7327b18cfb90cd05777d5839e7dad8085bb50
Francesco <inerba@gmail.com>
2023-11-22 15:11:21 +0100

    wip

--- resources/js/components/map-component.js
+++ resources/js/components/map-component.js
@@ -192,7 +192,7 @@ export default function mapComponent({
 
             // {{-- Quando si rimuove un layer --}}
             this.map.on("pm:remove", (e) => {
-                const gJg = this.geoJsonGroup.toGeoJSON();
+                const gJg = {...this.geoJsonGroup};
                 gJg.removeLayer(e);
                 this.$wire.set(this.geoJsonStatePath, JSON.stringify(gJg));
             });
